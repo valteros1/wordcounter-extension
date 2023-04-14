80d1d8d7f567849957a4ec960d98d6d49b141b68
valteros1 <79317752+valteros1@users.noreply.github.com>
2023-04-14 22:22:46 +0300

    saan vastuse evkk.tlu.ee console.logi

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # wordcounter-extension
+Chrome veebilaiend
--- content.js
+++ content.js
@@ -1,22 +1,23 @@
-function sendSelectedText(selectedText) {
-  
-  console.log(selectedText); // for debugging
-  fetch('http://localhost:3000/selected-text', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json' // Request body is in JSON format
-    },
-    body: JSON.stringify({ text: selectedText }) 
-  })
-  .then(response => response.json()) 
-  .then(data => console.log(`Word count: ${data.count}`)) 
-  .catch(error => console.error(error)); 
+async function sendSelectedText(selectedText) { // async funktsioon mis saadab valitud teksti http post requestina
+  try {
+    const response = await fetch("https://evkk.tlu.ee/api/texts/korrektuur", { // POST request evkk urli'le
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ tekst: selectedText }) 
+    });
+    const data = await response.json(); // 
+    console.log(data); 
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-
-  document.addEventListener('mouseup', function(event) {
-    const selectedText = window.getSelection().toString(); 
-    if (selectedText) { 
-      sendSelectedText(selectedText); 
-    }
-  });
+document.addEventListener('mouseup', function(event) { // 
+  const selectedText = window.getSelection().toString(); // võta valitud tekst ning muuda see stringiks
+  if (selectedText) {
+    sendSelectedText(selectedText);
+  }
+});
