7d025295bab026e3cd9d3818fb37742a84c03021
valteros1 <79317752+valteros1@users.noreply.github.com>
2023-05-14 23:04:45 +0300

    Lisatud vahelehed eksimused, tasemehinnang, keerukus, mitmekesisus. Vaikimisi kuvab Eksimuste vahelehte.

--- content.js
+++ content.js
@@ -1,3 +1,6 @@
+
+
+
 async function sendSelectedText(selectedText) {
   try {
     const response = await fetch("https://evkk.tlu.ee/api/texts/korrektuur", {
@@ -26,7 +29,15 @@ async function sendSelectedText(selectedText) {
       },
       body: JSON.stringify({ tekst: selectedText }) 
     });
-
+    
+    const responseTase = await fetch("https://evkk.tlu.ee/api/texts/keeletase", {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ tekst: selectedText }) 
+    });
 
 
 
@@ -37,28 +48,22 @@ async function sendSelectedText(selectedText) {
     const data = await response.json();
     const dataMitmekesisus = await responseMitmekesisus.json();
     const dataKeerukus = await responseKeerukus.json();
+    const dataTase = await responseTase.json();
     
     console.log(data); 
     console.log(dataMitmekesisus)
     console.log(dataKeerukus)
+    console.log(dataTase)
     
 
     const recievedWords = data[0];
     const sentText = data[1];
-    
-    console.log("sentText :", sentText)
-    console.log("recievedWords :", recievedWords)
+
+    // Tasemehinnangu osa
 
 
-    // Mitmekesisuse kontroll console.login vastavalt 
 
-    const howManyWords = dataMitmekesisus[11];
-    const differentWords = dataMitmekesisus[10];
-    console.log("Words :", howManyWords)
-    console.log("Different words :", differentWords)
-
-
-    // loen vastused Keerukusest konstanti
+    // Keerukuse osa
     const sentencesCount = dataKeerukus[0];
     const wordCount = dataKeerukus[1];
     const longSyllables = dataKeerukus[2];
@@ -68,9 +73,40 @@ async function sendSelectedText(selectedText) {
     const fkIndex = dataKeerukus[6];
     const lix = dataKeerukus[7];
     const sentenceLevel = dataKeerukus[9];
-    console.log("Sentences Count :", sentencesCount, "Word Count:", wordCount, "Long Syllable words:", longSyllables, "Syllables :",
-    syllablesCount, "long Word count", longWords, "SMOG", smog, "Flesh-Kincaidi index", fkIndex, "LIX", lix, "Pakutav keerukustase", sentenceLevel)
 
+
+    
+    console.log("sentText :", sentText)
+    console.log("recievedWords :", recievedWords)
+    console.log("wordCount :", wordCount)
+
+
+    // Mitmekesisuse kontroll console.login vastavalt 
+
+    const howManyWords = dataMitmekesisus[11];
+    const differentWords = dataMitmekesisus[10];
+    const klss = dataMitmekesisus[0];
+    const jlss = dataMitmekesisus[1];
+    const mtld = dataMitmekesisus[4];
+    const hdd = dataMitmekesisus[5];
+    console.log("Words :", howManyWords)
+    console.log("Different words :", differentWords)
+
+
+    //
+
+
+    // loen vastused Keerukusest konstanti
+    // const sentencesCount = dataKeerukus[0];
+    // const wordCount = dataKeerukus[1];
+    // const longSyllables = dataKeerukus[2];
+    // const syllablesCount = dataKeerukus[3];
+    // const longWords = dataKeerukus[4];
+    // const smog = dataKeerukus[5];
+    // const fkIndex = dataKeerukus[6];
+    // const lix = dataKeerukus[7];
+    // const sentenceLevel = dataKeerukus[9];
+    console.log("Sentences Count fro Complexity :", sentencesCount)
 
     const sentWords = sentText.split(' ');
 
@@ -82,7 +118,25 @@ async function sendSelectedText(selectedText) {
           data: {
             sentText: sentText,
             recievedWords: recievedWords,
-            sentWords: sentWords
+
+            sentWords: sentWords,
+            wordCount: wordCount,
+            sentencesCount: sentencesCount,
+            longSyllables: longSyllables,
+            syllablesCount: syllablesCount,
+            longWords: longWords,
+            smog: smog,
+            fkIndex: fkIndex,
+            lix: lix,
+            sentenceLevel: sentenceLevel,
+            howManyWords: howManyWords,
+            differentWords: differentWords,
+            klss: klss,
+            jlss: jlss,
+            mtld: mtld,
+            hdd: hdd
+
+
           }
         };
         
@@ -91,7 +145,23 @@ async function sendSelectedText(selectedText) {
           data: { 
             sentText: sentText,
             recievedWords: recievedWords, 
-            sentWords: sentWords 
+            sentWords: sentWords, 
+            wordCount: wordCount,
+            sentencesCount: sentencesCount,
+            longSyllables: longSyllables,
+            syllablesCount: syllablesCount,
+            longWords: longWords,
+            smog: smog,
+            fkIndex: fkIndex,
+            lix: lix,
+            sentenceLevel: sentenceLevel,
+            howManyWords: howManyWords,
+            differentWords: differentWords,
+            klss: klss,
+            jlss: jlss,
+            mtld: mtld,
+            hdd: hdd
+
           } 
         }, function(response) {
           console.log('Message sent from content.js to background.js');
@@ -102,6 +172,73 @@ async function sendSelectedText(selectedText) {
           chrome.storage.local.set({ 'sentText': sentText }, function() {
             console.log('Value is set to sentText' + data[1]);
           });
+
+          // Keerukuse endpoindi andmed salvestada Chrome localisse
+
+          chrome.storage.local.set({ 'sentencesCount': sentencesCount }, function() {
+            console.log('Value is set to sentencesCount' + dataKeerukus[0]);
+          });
+
+          chrome.storage.local.set({ 'wordCount': wordCount }, function() {
+            console.log('Value is set to wordCount' + dataKeerukus[1]);
+          });
+
+          chrome.storage.local.set({ 'longSyllables': longSyllables }, function() {
+            console.log('Value is set to longSyllables' + dataKeerukus[2]);
+          });
+
+          chrome.storage.local.set({ 'syllablesCount': syllablesCount }, function() {
+            console.log('Value is set to syllablesCount' + dataKeerukus[3]);
+          });
+
+          chrome.storage.local.set({ 'longWords': longWords }, function() {
+            console.log('Value is set to longWords' + dataKeerukus[4]);
+          });
+
+          chrome.storage.local.set({ 'smog': smog }, function() {
+            console.log('Value is set to smog' + dataKeerukus[5]);
+          });
+
+          chrome.storage.local.set({ 'fkIndex': fkIndex }, function() {
+            console.log('Value is set to fkIndex' + dataKeerukus[6]);
+          });
+
+          chrome.storage.local.set({ 'lix': lix }, function() {
+            console.log('Value is set to lix' + dataKeerukus[7]);
+          });
+
+          chrome.storage.local.set({ 'sentenceLevel': sentenceLevel }, function() {
+            console.log('Value is set to sentenceLevel' + dataKeerukus[9]);
+          });
+
+          chrome.storage.local.set({ 'howManyWords': howManyWords }, function() {
+            console.log('Value is set to howManyWords' + dataMitmekesisus[11]);
+          });
+
+          chrome.storage.local.set({ 'differentWords': differentWords }, function() {
+            console.log('Value is set to differentWords' + dataMitmekesisus[9]);
+          });
+
+          chrome.storage.local.set({ 'klss': klss }, function() {
+            console.log('Value is set to klss' + dataMitmekesisus[0]);
+          });
+
+          chrome.storage.local.set({ 'jlss': jlss }, function() {
+            console.log('Value is set to jlss' + dataMitmekesisus[2]);
+          });
+
+          chrome.storage.local.set({ 'mtld': mtld }, function() {
+            console.log('Value is set to mtld' + dataMitmekesisus[4]);
+          });
+
+          chrome.storage.local.set({ 'hdd': hdd }, function() {
+            console.log('Value is set to hdd' + dataMitmekesisus[5]);
+            console.log("mis kuradi asi on siis see", response.recievedWords)
+          });
+
+          
+
+
         });
       }, 1000);
     } else {
@@ -116,7 +253,8 @@ async function sendSelectedText(selectedText) {
 }
 
 
-let selectedText = "";
+
+
 document.addEventListener('mouseup', function(event) {
   chrome.runtime.sendMessage({ type: 'mouseUpEvent' });
   selectedText = window.getSelection().toString();
@@ -131,5 +269,11 @@ document.addEventListener('mouseup', function(event) {
     sendSelectedText(selectedText);
   }
 });
+  
 
 
+
+
+
+
+console.log("mis kuradi asi on siis see", response.recievedWords)
--- popup.js
+++ popup.js
@@ -2,66 +2,177 @@ document.addEventListener('DOMContentLoaded', function() {
   chrome.runtime.sendMessage({ type: 'requestData' }, function(response) {
     console.log('Message sent from popup.js to background.js');
     console.log(response);
-
+    hideAll()
+    document.getElementById('text-mistakes-button').style.backgroundColor="#fbcdfc";
+    document.getElementById('corrected-area').style.display = 'block';
     
-// If there are mistake, hide the corrected-words element and it displays the no-mistakes element
+
+    // If there are mistakes, hide the no-mistakes element and show the corrected-words element
     if (response && response.sentText && response.recievedWords && response.sentText === response.recievedWords) {
-      document.getElementById('no-mistakes').style.display = 'block';
-      document.getElementById('corrected-words').style.display = 'none';
+      // document.getElementById('no-mistakes').style.display = 'block';
+      // document.getElementById('corrected-words').style.display = 'block';
+      let test = 'Vigu ei leitud';
+      document.getElementById('corrected-words').innerHTML = test;
+      
+      // chrome.extension.getBackgroundPage().console.log('Teade, mida soovite kuvada');
     } else if (response && response.recievedWords) {
+      console.log("SEE ON ", response.recievedWords)
       const correctedWords = response.recievedWords.split(' ');
       const sentText = response.sentText.split(' ');
       let highlightedText = '';
+      
       for (let i = 0; i < correctedWords.length; i++) {
         if (correctedWords[i] !== sentText[i]) {
           highlightedText += `<span class="stripes">${sentText[i]}</span> <span style="background-color: #AEF359"> ${correctedWords[i]}</span>  `;
         } else {
           highlightedText += `${correctedWords[i]} `;
-        }
+                  }
       }
       // Display the highlighted text in the corrected-words element and hide the no-mistakes element
-      document.getElementById('corrected-words').style.display = 'block';
       document.getElementById('corrected-words').innerHTML = highlightedText;
-      document.getElementById('no-mistakes').style.display = 'none';
+      // document.getElementById('no-mistakes').style.display = 'none';
+      // document.getElementById('corrected-words').style.display = 'none'; // hide the corrected-words element initially
     } else {
-      // If there is no response, hide the corrected-words element and display the no-mistakes element
-      document.getElementById('no-mistakes').style.display = 'block';
-      document.getElementById('corrected-words').style.display = 'none';
+      let test = 'Tekst pole valitud';
+      // If there is no response, hide the corrected-words element and show the no-mistakes element
+      document.getElementById('corrected-words').innerHTML = test;
+      document.getElementById('corrected-words').style.display = 'block';
+      // document.getElementById('no-mistakes').style.display = 'block';
     }
+  });
 
-  
+  // Add a click event listener to the Mistakes button that shows the corrected-words element
+  const mistakesButton = document.getElementById('text-mistakes-button');
+  mistakesButton.addEventListener('click', () => {
+
+
+    hideAll();
+    document.getElementById('corrected-area').style.display = 'block';
+    document.getElementById('text-mistakes-button').style.backgroundColor="#fbcdfc";
+
+
+
+  // const correctedWordsEl = document.getElementById('corrected-words');
+  //    const noMistakesEl = document.getElementById('no-mistakes');
+  //    if (correctedWordsEl.style.display === 'none') {
+       
+  //      correctedWordsEl.style.display = 'block';
+  //      noMistakesEl.style.display = 'none';
+  //    } else {
+       
+  //      correctedWordsEl.style.display = 'none';
+  //      noMistakesEl.style.display = 'block';
+  //    }
+   });
+
+  const closeButton = document.getElementById('close-button');
+  closeButton.addEventListener('click', () => {
+    chrome.storage.local.clear();
+    // hideAll();
+
+    window.close();
+    // window.getSelection().removeAllRanges();
+
+  });
+
+  // const assassmentButton = document.getElementById('level-assessment-button');
+  // assassmentButton.addEventListener('click', () => {
+  //   window.close();
+  // });
+});
+
+document.getElementById('level-assessment-button').addEventListener('click', function() { 
+
+  hideAll();
+  document.getElementById('assessment-area').style.display = 'block';
+  document.getElementById('level-assessment-button').style.backgroundColor="#fbcdfc";
+
+
+})
+
+
+
+
+
+let complexityButton = document.getElementById('text-complexity-button');
+
+document.getElementById('text-complexity-button').addEventListener('click', function() {
+  chrome.storage.local.get(['wordCount', 'sentencesCount','longSyllables','syllablesCount','longWords','smog',
+  'fkIndex','lix','sentenceLevel'], function(result) {
+    const wordCount = result.wordCount
+    const sentencesCount = result.sentencesCount
+    const longSyllables = result.longSyllables
+    const syllablesCount = result.syllablesCount
+    const longWords = result.longWords
+    const smog = result.smog
+    const fkIndex = result.fkIndex
+    const lix = result.lix
+    const sentenceLevel = result.sentenceLevel
+    
+    // Kuna komadega tulevad SMOG ja FK Index, ümardan neid
+    const roundedSmog = Math.floor(smog * 100) / 100;
+    const roundedfkIndex = Math.floor(fkIndex * 100) / 100; 
+
+    hideAll();
+    document.getElementById('complexity-area').style.display = 'block';
+    document.getElementById('text-complexity-button').style.backgroundColor="#fbcdfc";
+
+    
+    
+    // Keerukuse
+    document.getElementById('complexity').innerHTML = "Keerukuse andmed" ;
+    document.getElementById('sentences').innerHTML = 'Lauseid: ' + sentencesCount;
+    document.getElementById('words').innerHTML = 'Sõnu: ' + wordCount;
+    document.getElementById('multisyllables').innerHTML = 'Paljusilbilisi sõnu: ' + longSyllables;
+    document.getElementById('syllables').innerHTML = 'Silpe: ' + syllablesCount;
+    document.getElementById('longwords').innerHTML = 'Pikki sõnu: ' + longWords;
+    document.getElementById('smog').innerHTML = 'SMOG: ' + roundedSmog;
+    document.getElementById('fkindex').innerHTML = 'Flesch-Kincaidi Indeks: ' + roundedfkIndex;
+    document.getElementById('lix').innerHTML = 'LIX: ' + lix;
+    document.getElementById('sentenceslevel').innerHTML = 'Pakutav keerukustase: ' + sentenceLevel;
+    
+
   });
 });
 
-// Add a click event listener to the button
+let diversityButton = document.getElementById('text-diversity-button');
 
-// 
-// const closeButton = document.getElementById('close-button');
-// closeButton.addEventListener('click', () => {
-//   window.close();
-// });
-
-document.addEventListener('DOMContentLoaded', function() {
-    const closeButton = document.getElementById('close-button');
-    closeButton.addEventListener('click', () => {
-      chrome.storage.local.clear();
-      
-      window.close();
-      window.getSelection().removeAllRanges();
-      
-    });
+document.getElementById('text-diversity-button').addEventListener('click', function() {
+  chrome.storage.local.get(['howManyWords', 'differentWords', 'klss', 'jlss', 'mtld', 'hdd'], function(result) {
+    const howManyWords = result.howManyWords
+    const differentWords = result.differentWords
+    const klss = result.klss
+    const jlss = result.jlss
+    const mtld = result.mtld
+    const hdd = result.hdd
     
+      
+    hideAll();
+    document.getElementById('diversity-area').style.display = 'block';
+    document.getElementById('text-diversity-button').style.backgroundColor="#fbcdfc";
+    // Mitmekesisus
+    document.getElementById('diversity').innerHTML = "Sõnavara mitmekesisuse andmed" ;
+    document.getElementById('diversitysentence').innerHTML = 'Sõnu: ' + howManyWords;
+    document.getElementById('differentwords').innerHTML = 'Lemmasid ehk erinevaid sõnu: ' + differentWords;
+    document.getElementById('klss').innerHTML = 'Korrigeeritud lemmade-sõnade suhtarv - KLSS(ingl Corrected Type-Token Ratio): ' + klss;
+    document.getElementById('jlss').innerHTML = 'Juuritud lemmade-sõnade suhtarv - JLSS(ingl Root Type-Token Ratio): ' + jlss;
+    document.getElementById('mtld').innerHTML = 'MTLD indeks(ingl Measure of Textual Lexical Diversity): ' + mtld;
+    document.getElementById('hdd').innerHTML = 'HDD indeks(ingl Hypergeometric Distribution D): ' + hdd;
+ 
+    
+
   });
+});
 
-  
-  document.addEventListener('DOMContentLoaded', function() {
-    const assassmentButton = document.getElementById('level-assessment-button');
-    assassmentButton.addEventListener('click', () => {
-      window.close();
-    });
-  });
+function hideAll(){
+  document.getElementById('complexity-area').style.display = 'none';
+  document.getElementById('assessment-area').style.display = 'none';
+  document.getElementById('diversity-area').style.display = 'none';
+  document.getElementById('corrected-area').style.display = 'none';
 
+  document.getElementById('text-complexity-button').style.backgroundColor="aliceblue";
+  document.getElementById('text-mistakes-button').style.backgroundColor="aliceblue";
+  document.getElementById('level-assessment-button').style.backgroundColor="aliceblue";
+  document.getElementById('text-diversity-button').style.backgroundColor="aliceblue";
 
-
-
-
+}
